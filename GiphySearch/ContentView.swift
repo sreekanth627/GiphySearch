51afd2d563b476b713b64d9efce80e4277b31198
sindhujaAmara <sindhujaa510@gmail.com>
2024-02-08 20:46:37 -0600

    Initial Commit

---
+++ GiphySearch/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  GiphySearch
+//
+//  Created by srikanth sanagapalli on 2/8/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ GiphySearch/GiphySearchApp.swift
@@ -0,0 +1,17 @@
+//
+//  GiphySearchApp.swift
+//  GiphySearch
+//
+//  Created by srikanth sanagapalli on 2/8/24.
+//
+
+import SwiftUI
+
+@main
+struct GiphySearchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
